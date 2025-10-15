d4f3d536a4227840050df3cf3e2a9f6977c297f4
Federico Pi Dote <fpidote@hotmail.com>
2025-10-15 21:09:42 +0200

    Added photo Collection files

---
+++ models/photo.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const photoSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String },
+  imageUrl: { type: String, required: true },
+  publicId: { type: String }, // ID de Cloudinary
+  likes: { type: Number, default: 0 },
+  //likedBy: [{ type: String }], // opcional: emails o IDs anónimos- No estoy seguro si es conveniente
+  createdAt: { type: Date, default: Date.now },
+});
+
+export default mongoose.model("Photo", photoSchema);
