d935e3c559eb5a41ed0b214a567d3327bebea47c
Samir Gavilanes <thesamnes@gmail.com>
2025-12-18 19:03:35 -0500

    Revert "Test mosaic upload"
    This reverts commit 27fde89a216dfc05fcb2d586cfe4bc645606d6fb.

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -4,12 +4,11 @@ import cloudinary from "../config/cloudinary.js";
 import Photo from "../models/photo.js";
 import { Readable } from "stream";
 
-
 dotenv.config();
 
 export const completeRegistration = async (req, res) => {
   try {
-    const { email, name, age, country, story, photoYear, title, width, height, colors } = req.body;
+    const { email, name, age, country, story, photoYear, title } = req.body;
 
     if (!email) {
       return res.status(400).json({ message: "Falta el correo electrónico." });
@@ -64,9 +63,6 @@ export const completeRegistration = async (req, res) => {
       photoYear,
       photos: uploadedUrls,
       subscribedAt: new Date(),
-      width: uploadedAssets[0]?.width || null,
-      height: uploadedAssets[0]?.height || null,
-      colors: uploadedAssets[0]?.colors || [],
     });
 
     await newUser.save();
@@ -85,9 +81,6 @@ export const completeRegistration = async (req, res) => {
       imageUrl: asset.url,
       publicId: asset.publicId,
       year: normalizedYear,
-      width : asset.width || null,
-      height : asset.height || null,
-      colors : asset.colors || []
     }));
 
     await Photo.insertMany(photoDocs);
@@ -209,9 +202,6 @@ export const getUserPhotos = async (req, res) => {
         hidden: Boolean(photo.hidden),
         likes: photo.likes,
         createdAt: photo.createdAt,
-        width: photo.width,
-        height: photo.height,
-        colors: photo.colors
       }));
     } else {
       // Fallback legacy data
@@ -228,9 +218,6 @@ export const getUserPhotos = async (req, res) => {
         description: "",
         hidden: hiddenLegacy.includes(url),
         likes: 0,
-        width: null,
-        height: null,
-        colors: [],
       }));
     }
 
--- models/EmailEntry.js
+++ models/EmailEntry.js
@@ -1,8 +1,6 @@
 import mongoose from "mongoose";
-extractColor (imageBuffer, colorCount)
 
 const { Schema } = mongoose;
-const colors = await extractColors(req.file.buffer);
 
 const emailEntrySchema = new Schema({
   email: { type: String, required: true, unique: true },
@@ -14,9 +12,6 @@ const emailEntrySchema = new Schema({
   photos: [{ type: String }],
   hiddenPhotos: { type: [String], default: [] },
   subscribedAt: { type: Date, default: Date.now },
-  width: Result.width,
-  height: Result.height,
-  colors: colors
 });
 
 export default mongoose.model("EmailEntry", emailEntrySchema);
